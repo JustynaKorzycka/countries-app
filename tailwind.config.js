47403f2223e8c2f23cd8b9170ef69741a64919d8
Justyna <justyna.korzycka@gmail.com>
2024-01-17 08:17:22 +0100

    add custom colors  to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+ content: [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ ],
+ theme: {
+  extend: {
+   screens: {
+    md: "54rem",
+   },
   },
-  plugins: [],
-}
+  colors: {
+   primary: "#111517",
+   secondary: "#2B3844",
+   searchColor: "#848484",
+   white: "#fff",
+   lightGray: "#F2F2F2",
+  },
+ },
+ plugins: [],
+};
