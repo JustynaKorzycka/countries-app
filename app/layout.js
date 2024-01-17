3a37ef5655ec2af8420ac4b2c94ab028c54bd492
Justyna <justyna.korzycka@gmail.com>
2024-01-17 21:28:25 +0100

    create theme provider, add theme provider to layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./themeProvider";
 import Header from "@/components/Header";
 
 const nunioSans = Nunito_Sans({
@@ -16,9 +17,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
   <html lang="en">
-   <body className={`${nunioSans.className} bg-lightGray text-primary`}>
-    <Header />
-    <main className="container py-6 sm:py-14">{children}</main>
+   <body
+    className={`${nunioSans.className} bg-lightGray dark:bg-dark text-primary dark:text-white `}
+   >
+    <ThemeProvider
+     attribute="class"
+     defaultTheme="system"
+     enableSystem
+    >
+     <Header />
+     <main className="container py-6 sm:py-14">{children}</main>
+    </ThemeProvider>
    </body>
   </html>
  );
