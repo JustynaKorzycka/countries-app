4231654a42a7c2710fd7604b3d310da04006ceac
Justyna <justyna.korzycka@gmail.com>
2024-01-17 21:04:47 +0100

    change Link in country component

--- components/Country.jsx
+++ components/Country.jsx
@@ -6,11 +6,11 @@ import DescriptionElement from "./DescriptionElement";
 const Country = ({ country }) => {
  if (country) {
   return (
-   <Link
-    href={`/${country.name}`}
-    className="flex flex-col w-64 bg-white drop-shadow-md hover:drop-shadow-2xl justify-self-center  rounded-md"
-   >
-    <div className="relative w-full h-40 ">
+   <div className="flex flex-col w-64 bg-white drop-shadow-md hover:drop-shadow-2xl justify-self-center  rounded-md">
+    <Link
+     href={`/${country.name}`}
+     className="relative w-full h-40 "
+    >
      <Image
       src={country.flag}
       alt=""
@@ -18,7 +18,7 @@ const Country = ({ country }) => {
       style={{ objectFit: "cover" }}
       className="rounded-md"
      />
-    </div>
+    </Link>
     <div className="p-6 pb-9 flex flex-col min-h-44">
      <h2 className="grow">{country.name}</h2>
      <div className="">
@@ -36,7 +36,7 @@ const Country = ({ country }) => {
       />
      </div>
     </div>
-   </Link>
+   </div>
   );
  }
 };
