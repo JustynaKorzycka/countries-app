af8b90099b7cee21279de90e78b1c5af0fa1c6af
Justyna <justyna.korzycka@gmail.com>
2024-01-18 11:26:41 +0100

    change link in borders: replace white spaces

--- components/CountryDesc/Borders.jsx
+++ components/CountryDesc/Borders.jsx
@@ -5,15 +5,18 @@ const Borders = ({ borders }) => {
   <div className="mt-8 max-w-xl md:flex  md:flex-wrap md:items-center md:gap-y-4 md:gap-x-4">
    <h3 className="text-base font-semibold mb-4 md:mb-0">Border Countries:</h3>
    <div className="grid grid-cols-3 sm:grid-cols-5 gap-2.5 md:flex  md:flex-wrap">
-    {borders.map((border, index) => (
-     <Link
-      href={`/country/${border}`}
-      key={index}
-      className=" drop-shadow-lg bg-white dark:bg-lightDark py-2 text-center text-xs md:text-sm md:px-3 rounded-sm break-all"
-     >
-      {border}
-     </Link>
-    ))}
+    {borders.map((border, index) => {
+     const correctBorder = border.replace(/ /g, "%20");
+     return (
+      <Link
+       href={`/country/${correctBorder}`}
+       key={index}
+       className=" drop-shadow-lg bg-white dark:bg-lightDark py-2 text-center text-xs md:text-sm md:px-3 rounded-sm break-all"
+      >
+       {border}
+      </Link>
+     );
+    })}
    </div>
   </div>
  );
